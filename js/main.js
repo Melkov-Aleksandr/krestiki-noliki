a87649ddd7f9835ca81d1e7833d7561243c071dc
Александр <melkov.1994@list.ru>
2021-11-25 23:24:23 +0500

    Готовые крестики-нолики

--- js/main.js
+++ js/main.js
@@ -14,93 +14,103 @@ const winLayout = [
     [3,5,7],
 ];
 
+const returnPlayer = (cb) => {
+    player = cb;
+    currentPlayer.innerHTML = player.toUpperCase();
+    return player;
+}
+
+const meassegeClose = () => {
+    setTimeout(function(){
+        meassege.classList.add('visually-hidden');
+    }, 800);
+};
+
+function checkWin(data) {
+    for (var i in winLayout) {
+        var win = true;
+        for (var j in winLayout[i]) {
+            var id = winLayout[i][j];
+            var ind = data.indexOf(id);
+
+            if (ind == -1) {
+                win = false
+            }
+        }
+
+        if (win) {
+            return true;
+        }
+    }
+    return false;
+};
+
+function restart(text) {
+    meassege.querySelector('.meassege-text').textContent = text;
+    meassege.classList.remove('visually-hidden');
+    meassegeClose();
+
+    for (var i = 0; i < cell.length; i++) {
+        cell[i].innerHTML = '';
+    }
+}
+
 function cellClick() {
 
     var data = [];
     
     if (!this.innerHTML) {
         this.innerHTML = player;
+
+        for (var i in cell) {
+            if (cell[i].innerHTML == player) {
+                data.push(parseInt(cell[i].getAttribute('pos')));
+            }
+        }
+    
+        if (checkWin(data)) {
+            setTimeout(function() {
+                if (player == 'o') {
+                    player = 'x';
+                    returnPlayer(player);
+                    restart("Выграл: " + player);
+                } else {
+                    player = 'o';
+                    returnPlayer(player);
+                    restart("Выграл: " + player);
+                }
+            }, 100)
+        } else {
+            var draw = true;
+            for (var i in cell) {
+                if(cell[i].innerHTML == '') draw = false;
+            }
+            if (draw) {
+                const textMeassege = 'Ничья'
+                setTimeout(function() {
+                    restart(textMeassege);
+                }, 100)
+            }
+        }
+        
+        if (player == 'x') {
+            player = 'o';
+            currentPlayer.innerHTML = player.toUpperCase();
+            return player;
+        } else {
+            player = 'x';
+            currentPlayer.innerHTML = player.toUpperCase();
+            return player;
+        }
+
     } else {
-        const meassegeText = 'Занято'
+        const meassegeText = 'Занято';
         meassege.classList.remove('visually-hidden');
         meassege.querySelector('.meassege-text').textContent = meassegeText;
         meassegeClose();
     }
-
-    for (var i in cell){
-        if (cell[i].innerHTML == player){
-            data.push(parseInt(cell[i].getAttribute('pos')));
-        }
-    }
-
-    if (checkWin(data)) {
-        setTimeout(function() {
-            if (player == 'o') {
-                player = 'x';
-                restart("Выграл: " + player);
-            } else {
-                player = 'o';
-                restart("Выграл: " + player);
-            }
-        }, 100)
-    } else {
-        var draw = true;
-        for (var i in cell) {
-            if(cell[i].innerHTML == '') draw = false;
-        }
-        if (draw) {
-            const textMeassege = 'Ничья'
-            setTimeout(function() {
-                restart(textMeassege);
-            }, 100)
-        }
-    }
-    
-    if (player == 'x') {
-        player = 'o';
-        currentPlayer.innerHTML = player.toUpperCase();
-        return player;
-    } else {
-        player = 'x';
-        currentPlayer.innerHTML = player.toUpperCase();
-        return player;
-    }
-
 }
-
-function restart(text) {
-    meassege.querySelector('.meassege-text').textContent = text;
-    meassege.classList.remove('visually-hidden');
-    meassegeClose();
-    player = 'x';
-    for (var i = 0; i < cell.length; i++) {
-        cell[i].innerHTML = '';
-    }
-}
-
-function checkWin(data) {
-    for (var i in winLayout) {
-        var win = true;
-        for(var j in winLayout[i]) {
-            var id = winLayout[i][j];
-            var ind = data.indexOf(id);
-
-            if(ind == -1) {
-                win = false
-            }
-        }
-
-        if(win) return true;
-    }
-    return false;
-};
 
 for (let i = 0; i < cell.length; i++) {
     cell[i].addEventListener('click', cellClick, false);
-};
-
-const meassegeClose = () => {
-    setTimeout(function(){
-        meassege.classList.add('visually-hidden');
-    }, 800);
 };
